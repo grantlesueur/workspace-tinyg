5f7d49c0a9353972196399e23058012480b298f6
grantlesueur <grantlesueur@gmail.com>
2019-03-11 02:15:08 +0000

    Made some changes to ChiliPeppr myWorkspace using Cloud9

--- workspace.js
+++ workspace.js
@@ -1071,7 +1071,7 @@ cpdefine("inline:com-chilipeppr-workspace-tinyg", ["chilipeppr_ready"], function
                         var that = this;
                         chilipeppr.load(
                             "#com-chilipeppr-ws-superTouchplate",
-                            "http://raw.githubusercontent.com/grantlesueur/widget-super-touchplate/auto-generated-widget.html",
+                            "https://raw.githubusercontent.com/grantlesueur/widget-super-touchplate/master/auto-generated-widget.html",
                             //"http://raw.githubusercontent.com/PyroAVR/widget-super-touchplate/master/auto-generated-widget.html",
                             // "http://raw.githubusercontent.com/PyroAVR/widget-super-touchplate/tabs/auto-generated-widget.html",
                             function() {
